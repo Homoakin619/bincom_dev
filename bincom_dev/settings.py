677f6c917e4b10112e859e9140e9b04fab7a038f
Homoakin619 <homohaykins619@gmail.com>
2022-11-19 11:03:16 +0100

    static files changes

--- bincom_dev/settings.py
+++ bincom_dev/settings.py
@@ -18,7 +18,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import os
 
 env = environ.Env()
 environ.Env.read_env()
